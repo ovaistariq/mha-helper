3b5412f1fb385cff1fac32d10e0cdf8daaf28c8d
Ovais Tariq <ovaistariq@gmail.com>
2013-06-07 17:37:26 +0200

    Add support for the new options that MHA passes to the ip_online_change helper

--- scripts/master_ip_online_change.py
+++ scripts/master_ip_online_change.py
@@ -7,11 +7,12 @@ from lib.mha_online_change_helper import MHA_online_change_helper
 # set some config variables
 privileged_users = ['root']
 
-# /usr/local/mha-helper/scripts/master_ip_online_change.py 
-# --command=stop --orig_master_host=master1 --orig_master_ip=10.20.111.6 
-# --orig_master_port=3306 --new_master_host=slave1 
-# --new_master_ip=10.20.102.5 --new_master_port=3306  
-# --ssh_options='-o ServerAliveInterval=60 -o ServerAliveCountMax=20 -o StrictHostKeyChecking=no -o ConnectionAttempts=5 -o PasswordAuthentication=no -i /root/.ssh/id_rsa'
+#/usr/local/mha-helper/scripts/master_ip_online_change.py \
+#        --command=stop --orig_master_host=repl01 --orig_master_ip=192.168.122.151 
+#        --orig_master_port=3306 --orig_master_user='mha_helper' --orig_master_password='xxx' 
+#        --new_master_host=repl02 --new_master_ip=192.168.122.152 --new_master_port=3306 
+#        --new_master_user='mha_helper' --new_master_password='xxx'  
+#        --ssh_options='-o ServerAliveInterval=60 -o ServerAliveCountMax=20 -o StrictHostKeyChecking=no -o ConnectionAttempts=5 -o PasswordAuthentication=no -o BatchMode=yes -i /root/.ssh/id_rsa' 
 
 # parse comand line arguments
 parser = OptionParser()
@@ -19,9 +20,13 @@ parser.add_option('--command', type='string')
 parser.add_option('--orig_master_host', type='string')
 parser.add_option('--orig_master_ip', type='string')
 parser.add_option('--orig_master_port', type='string')
+parser.add_option('--orig_master_user', type='string')
+parser.add_option('--orig_master_password', type='string')
 parser.add_option('--new_master_host', type='string')
 parser.add_option('--new_master_ip', type='string')
 parser.add_option('--new_master_port', type='string')
+parser.add_option('--new_master_user', type='string')
+parser.add_option('--new_master_password', type='string')
 parser.add_option('--ssh_options', type='string')
 
 (options, args) = parser.parse_args()
