e29eda70420242576dce0fd90a24d39b2a761d76
Ovais Tariq <ovaistariq@gmail.com>
2013-06-08 10:32:53 +0200

    Add an option for specifying IP address in CIDR notification as that is needed by 'ip' command

--- scripts/lib/mha_vip_helper.py
+++ scripts/lib/mha_vip_helper.py
@@ -5,13 +5,13 @@ class MHA_VIP_helper(object):
     @staticmethod
     def remove_vip(config_helper, host_ip, ssh_user, ssh_options):
         cluster_interface = config_helper.get_cluster_interface()
-        writer_vip = config_helper.get_writer_vip()
+        writer_vip_cidr = config_helper.get_writer_vip_cidr()
 
         if ssh_user == None:
            ssh_user = config_helper.get_ssh_user()
 
         ip_cmd = "%s addr delete %s dev %s" % (MHA_config_helper.IP, 
-                                            writer_vip, cluster_interface)
+                                            writer_vip_cidr, cluster_interface)
 
         if config_helper.get_requires_sudo() == True:
             ip_cmd = "%s %s" % (MHA_config_helper.SUDO, ip_cmd)
@@ -30,13 +30,14 @@ class MHA_VIP_helper(object):
     @staticmethod
     def assign_vip(config_helper, host_ip, ssh_user, ssh_options):
         cluster_interface = config_helper.get_cluster_interface()
+        writer_vip_cidr = config_helper.get_writer_vip_cidr()
         writer_vip = config_helper.get_writer_vip()
 
         if ssh_user == None:
             ssh_user = config_helper.get_ssh_user()
 
         ip_cmd = "%s addr add %s dev %s" % (MHA_config_helper.IP, 
-                                            writer_vip, cluster_interface)
+                                        writer_vip_cidr, cluster_interface)
 
         arping_cmd = "%s -q -c 3 -A -I %s %s" % (MHA_config_helper.ARPING,
                                         cluster_interface, writer_vip)
