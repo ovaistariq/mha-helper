d918e290523c85fbfe08dab279350195ce27ee11
Ovais Tariq <ovaistariq@gmail.com>
2015-02-23 16:56:06 -0800

    Add testing related stuff

---
+++ tests/test_mha-helper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_mha-helper
+----------------------------------
+
+Tests for `mha-helper` module.
+"""
+
+import unittest
+
+from mha-helper import mha-helper
+
+
+class TestMha-helper(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
