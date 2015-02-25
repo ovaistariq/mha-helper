71cb83a24abca01d23a996f82e37fff211f44187
Ovais Tariq <ovaistariq@gmail.com>
2015-02-24 19:48:14 -0800

    Use pymysql instead of MySQLdb, a number of fixes to the mysql_helper class, add nose as a requirement

--- mha_helper/mysql_helper.py
+++ mha_helper/mysql_helper.py
@@ -15,73 +15,79 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import MySQLdb
+from __future__ import print_function
+import pymysql
 import re
 
-class MySQL_helper(object):
-    def __init__(self, host, user, password):
+
+class MySQLHelper(object):
+    @staticmethod
+    def is_read_only_query(sql):
+        if re.match('^select', sql, re.I) is not None:
+            return True
+
+        if re.match('^show', sql, re.I) is not None:
+            return True
+
+        return False
+
+    def __init__(self, host, port, user, password):
         self._host = host
+        self._port = port
         self._user = user
         self._password = password
+        self._connection = None
 
     def connect(self):
         try:
-            self._connection = MySQLdb.connect(host=self._host, user=self._user, passwd=self._password)
-        except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+            self._connection = pymysql.connect(host=self._host, port=self._port, user=self._user, passwd=self._password)
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
 
         return True
 
     def disconnect(self):
-        self._connection.close()
-
-    def is_read_only_query(self, sql):
-        if re.match('^select', sql, re.I) != None:
-            return True
-
-        if re.match('^show', sql, re.I) != None:
-            return True
-
-        return False
+        if self._connection is not None:
+            self._connection.close()
 
     def get_version(self):
         cursor = self._connection.cursor()
         try:
             cursor.execute("SELECT VERSION()")
             row = cursor.fetchone()
-        except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
-	    cursor.close()
+            cursor.close()
 
         return row[0]
 
     def get_connection_id(self):
-	return self._connection.thread_id()
+        return self._connection.thread_id()
 
     def get_current_user(self):
         return self._user
 
     def set_read_only(self):
         cursor = self._connection.cursor()
-	try:
+        try:
             cursor.execute("SET GLOBAL read_only = 1")
-	except MySQLdb.Error as e:
-	    print "Error %d: %s" % (e.args[0], e.args[1])
-	    return False
-	finally:
-	    cursor.close()
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
+            return False
+        finally:
+            cursor.close()
 
         return True
 
     def unset_read_only(self):
         cursor = self._connection.cursor()
-	try:
+        try:
             cursor.execute("SET GLOBAL read_only = 0")
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
@@ -89,22 +95,22 @@ class MySQL_helper(object):
         return True
 
     def is_read_only(self):
-	cursor = self._connection.cursor()
-	cursor.execute("SELECT @@read_only")
-	row = cursor.fetchone()
-	cursor.close()
+        cursor = self._connection.cursor()
+        cursor.execute("SELECT @@read_only")
+        row = cursor.fetchone()
+        cursor.close()
 
-	if row[0] == 1:
-	    return True
+        if row[0] == 1:
+            return True
 
-	return False
+        return False
 
     def disable_log_bin(self):
         cursor = self._connection.cursor()
-	try:
-            cursor.execute("SET SQL_LOG_BIN = 0")
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        try:
+                cursor.execute("SET SQL_LOG_BIN = 0")
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
@@ -115,8 +121,8 @@ class MySQL_helper(object):
         cursor = self._connection.cursor()
         try:
             cursor.execute("SET SQL_LOG_BIN = 1")
-        except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
@@ -124,53 +130,65 @@ class MySQL_helper(object):
         return True
 
     def get_processlist(self):
-	cursor = self._connection.cursor(MySQLdb.cursors.DictCursor)
-	try:
-	    cursor.execute("SHOW PROCESSLIST")
-	    threads = cursor.fetchall()
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        cursor = self._connection.cursor(pymysql.cursors.DictCursor)
+        try:
+            cursor.execute("SHOW PROCESSLIST")
+            threads = cursor.fetchall()
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
-	    cursor.close()
+            cursor.close()
 
-	return threads
+        return threads
 
     def get_all_users(self):
-	cursor = self._connection.cursor(MySQLdb.cursors.DictCursor)
-	try:
-	    cursor.execute("SELECT * from mysql.user")
-	    users = cursor.fetchall()
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        cursor = self._connection.cursor(pymysql.cursors.DictCursor)
+        try:
+            cursor.execute("SELECT * from mysql.user")
+            users = cursor.fetchall()
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
 
-	return users
+        return users
 
-    def get_user_grants(self, username):
-	cursor = self._connection.cursor()
+    def get_user_grants(self, username, hostname):
+        cursor = self._connection.cursor()
         grants = []
-	try:
-            cursor.execute("SHOW GRANTS FOR %s" % username)
+        try:
+            cursor.execute("SHOW GRANTS FOR '%s'@'%s'" % (username, hostname))
             for row in cursor.fetchall():
                 grants.append(row[0])
-        except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
 
-	return grants
+        return grants
+
+    def revoke_all_privileges(self, user, hostname):
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute(print("REVOKE ALL PRIVILEGES, GRANT OPTION FROM '%s'@'%s'" % (user, hostname)))
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
+            return False
+        finally:
+            cursor.close()
+
+        return True
 
     def get_slave_status(self):
-        cursor = self._connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor = self._connection.cursor(pymysql.cursors.DictCursor)
         try:
             cursor.execute("SHOW SLAVE STATUS")
             row = cursor.fetchone()
-        except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
@@ -178,37 +196,25 @@ class MySQL_helper(object):
         return row
 
     def kill_connection(self, connection_id):
-	cursor = self._connection.cursor()
-	try:
-	    cursor.execute("KILL CONNECTION %d" % connection_id)
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute("KILL CONNECTION %d" % connection_id)
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
 
-	return True
-
-    def revoke_all_privileges(self, user):
-	cursor = self._connection.cursor()
-	try:
-	    cursor.execute("REVOKE ALL PRIVILEGES, GRANT OPTION FROM %s" % user)
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
-            return False
-        finally:
-            cursor.close()
-
-	return True
+        return True
 
     def execute_admin_query(self, sql):
-	cursor = self._connection.cursor()
-	try:
-	    cursor.execute(sql)
-	except MySQLdb.Error as e:
-            print "Error %d: %s" % (e.args[0], e.args[1])
+        cursor = self._connection.cursor()
+        try:
+            cursor.execute(sql)
+        except pymysql.Error as e:
+            print("Error %d: %s" % (e.args[0], e.args[1]))
             return False
         finally:
             cursor.close()
 
-	return True
+        return True
