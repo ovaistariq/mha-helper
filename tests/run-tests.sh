e6e5c18a78235b085c7529949e13f3d9c8d274a3
Ovais Tariq <ovaistariq@gmail.com>
2015-03-09 13:48:16 -0700

    Remove MySQL sandbox creation code from the test runner, instead it now relies on a MySQL instance that it can connect to. Also tied in the ConfigHelper and SSHHelper unit tests into the script

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,44 +3,20 @@ set -e
 
 script_root=$(dirname $(readlink -f $0))
 
-mysql_version="5.6.19"
-mysql_binary_path="/Users/ovais.tariq/.mysql/binaries/mysql-5.6.19-osx10.7-x86_64.tar.gz"
-mysql_sandbox_path="/Users/ovais.tariq/sandboxes/msb_5_6_19"
+# Export the variables to be used by the MySQLHelper tests
+export MYSQL_TEST_VERSION="5.6.19-log"
+export MYSQL_TEST_IP="192.168.30.11"
+export MYSQL_TEST_USER="msandbox"
+export MYSQL_TEST_PASSWORD="msandbox"
+export MYSQL_TEST_PORT="3306"
 
-clean_exit() {
-    local error_code="$?"
-    kill -9 $(jobs -p) >/dev/null 2>&1 || true
-    (cd ${mysql_sandbox_path} && ./clear)
-    return ${error_code}
-}
-
-check_for_cmd () {
-    if ! which "$1" >/dev/null 2>&1
-    then
-        echo "Could not find $1 command" 1>&2
-        exit 1
-    fi
-}
-
-check_for_cmd make_sandbox
-
-trap "clean_exit" EXIT
-
-# Create a MySQL sandbox for tests
-make_sandbox ${mysql_binary_path} -- --no_confirm --force
-
-# Fetch the MySQL config parameters
-mysql_user=$(grep user ${mysql_sandbox_path}/my.sandbox.cnf -m 1 | cut -d '=' -f 2 | awk '{print $1}')
-mysql_pass=$(grep password ${mysql_sandbox_path}/my.sandbox.cnf -m 1 | cut -d '=' -f 2 | awk '{print $1}')
-mysql_port=$(grep port ${mysql_sandbox_path}/my.sandbox.cnf -m 1 | cut -d '=' -f 2 | awk '{print $1}')
-mysql_host=$(grep bind-address ${mysql_sandbox_path}/my.sandbox.cnf -m 1 | cut -d '=' -f 2 | awk '{print $1}')
-
-# Export the variables to be used by the tests
-export MYSQL_TEST_VERSION="${mysql_version}"
-export MYSQL_TEST_IP="${mysql_host}"
-export MYSQL_TEST_USER="${mysql_user}"
-export MYSQL_TEST_PASSWORD="${mysql_pass}"
-export MYSQL_TEST_PORT="${mysql_port}"
+# Export the variables that are needed by the SSHHelper test
+export SSH_TEST_HOST="master.localhost"
+export SSH_TEST_IP="192.168.30.11"
+export SSH_TEST_USER="root"
+export SSH_TEST_PORT="22"
 
 # Run the tests
 python ${script_root}/test_mysql_helper.py -v
+python ${script_root}/test_config_helper.py -v
+python ${script_root}/test_ssh_helper.py -v
