5de51fa259f455552eca837d74d3884a8d228ca9
Ovais Tariq <ovaistariq@gmail.com>
2016-02-06 22:40:26 -0800

    Add new options to mock config

--- tests/integration/test/integration/manager/serverspec/default_spec.rb
+++ tests/integration/test/integration/manager/serverspec/default_spec.rb
@@ -20,12 +20,6 @@ describe 'mha4mysql-manager package is installed' do
   end
 end
 
-describe 'python-mha_helper package is installed' do
-  describe package('python-mha_helper') do
-    it { should be_installed }
-  end
-end
-
 describe 'MySQL replication is setup correctly and MHA can reach MySQL and SSH' do
   describe command("/usr/bin/masterha_check_repl --conf /etc/mha/test_pod.conf") do
     its(:stdout) { should match /MySQL Replication Health is OK./ }
